ea7e19441f7cc6351dd1015a0d7b1e08798d2c83
Roland Edouard Jean Laurès <roland@liant.dev>
2025-07-08 14:07:58 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## [Unreleased]
+
+## [0.0.1] - 2025-07-08
+
+- Initial release
+- Unusable version
---
+++ lib/multiborg.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "multiborg/version"
+
+module Multiborg
+  class Error < StandardError; end
+  # Your code goes here...
+end
